3659b17c7514dc794ec7c9934d3d697424c4c577
thijsrijkers <thijsrijkers@live.nl>
2021-10-09 21:21:57 +0200

    +Disassembled sm rom with DiztinGUIsh

---
+++ src/labels.asm
@@ -0,0 +1,4 @@
+                       Native_COP = $008573                 ;      |        |      ;  
+                       Native_NMI = $009583                 ;      |        |      ;  
+                       Native_IRQ = $00986A                 ;      |        |      ;  
+                       Emulation_RESET = $00841C            ;      |        |      ;  
